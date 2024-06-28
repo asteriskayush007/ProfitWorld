da86c4451882384bdde85be775a69f812828aa22
Ayushmaan Yadav <143776285+asteriskayush007@users.noreply.github.com>
2024-06-28 12:32:09 +0530

    first commit

--- forms/contact.js
+++ forms/contact.js
@@ -3,13 +3,13 @@ function gotowhatsappmain() {
 var name = document.getElementById("name").value;
 var email = document.getElementById("email").value;
 var service = document.getElementById("service").value;
-var message = document.getElementById("message-field").value;
+var msg = document.getElementById("message").value;
 
 var url = "https://wa.me/918789529215?text="
     + "Name: " + name + "%0a"
     + "Email: " + email + "%0a"
-    + "Service: " + service;
-    + "Message: " + message;
+    + "Service: " + service + "%0a"
+    + "Message" + msg;
 
 
 window.open(url, '_blank').focus();
